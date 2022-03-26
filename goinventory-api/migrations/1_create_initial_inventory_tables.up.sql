eefd8425c89ca1b0f2b8c1ae29960541fb78a6aa
Yongle Xu <yonglexu08@gmail.com>
2022-03-26 22:06:14 +0000

    Updated the migration file to alter the data attributes.
    Count has been updated to not null has been added because that the count can be nullable. Same is true for the created_ at field. This value will be created on insert so should not be null.
    
    The count default 0 has been removed because golang the defined empty value for type int is 0 already so we do not need this attribute on the field.

--- goinventory-api/migrations/1_create_initial_inventory_tables.up.sql
+++ goinventory-api/migrations/1_create_initial_inventory_tables.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE inventory_list (
     id UUID PRIMARY KEY,
     name text NOT NULL,
     description TEXT,
-    created_at TIMESTAMP,
+    created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP
 );
 
@@ -10,7 +10,7 @@ CREATE TABLE inventory_item (
     id UUID PRIMARY KEY,
     inventory_list_id UUID NOT NULL REFERENCES inventory_list(id) ON DELETE CASCADE,
     name TEXT NOT NULL,
-    count INT DEFAULT 0,
-    created_at TIMESTAMP,
+    count INT NOT NULL,
+    created_at TIMESTAMP NULL,
     updated_at TIMESTAMP
 );
