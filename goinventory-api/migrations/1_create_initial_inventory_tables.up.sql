0736c2305f4bf3b6a606fdde667e2a65c5fe0dff
Yongle Xu <yonglexu08@gmail.com>
2022-04-01 14:23:54 +0100

    Revert first migration up file.
    Revert migration file to main because the migrations will not be applied. Should be part of a separate migration file.

--- goinventory-api/migrations/1_create_initial_inventory_tables.up.sql
+++ goinventory-api/migrations/1_create_initial_inventory_tables.up.sql
@@ -1,9 +1,9 @@
 CREATE TABLE inventory_list (
-    id UUID PRIMARY KEY NOT NULL,
+    id UUID PRIMARY KEY,
     name text NOT NULL,
     description TEXT,
-    created_at BIGINT NOT NULL,
-    updated_at BIGINT
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
 );
 
 CREATE TABLE inventory_item (
@@ -11,6 +11,6 @@ CREATE TABLE inventory_item (
     inventory_list_id UUID NOT NULL REFERENCES inventory_list(id) ON DELETE CASCADE,
     name TEXT NOT NULL,
     count INT NOT NULL,
-    created_at BIGINT NOT NULL,
-    updated_at BIGINT NOT NULL
+    created_at TIMESTAMP NULL,
+    updated_at TIMESTAMP
 );
