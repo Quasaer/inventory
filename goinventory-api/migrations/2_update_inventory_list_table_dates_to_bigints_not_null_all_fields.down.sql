562d40c42ba2ed2899ab9e383a2c54fc26dc1d53
Yongle Xu <yonglexu08@gmail.com>
2022-04-01 14:49:27 +0100

    Correct the postgres syntax for alter column
    Build failed due to syntax errors for migration. I needed to add type key word for the alter column and the set key word to add the not null contraint.

--- goinventory-api/migrations/2_update_inventory_list_table_dates_to_bigints_not_null_all_fields.down.sql
+++ goinventory-api/migrations/2_update_inventory_list_table_dates_to_bigints_not_null_all_fields.down.sql
@@ -1,4 +1,4 @@
-ALTER TABLE inventory_list ALTER created_at TIMESTAMP NOT NULL;
-ALTER TABLE inventory_list ALTER updated_at TIMESTAMP;
-ALTER TABLE inventory_item ALTER created_at TIMESTAMP NOT NULL;
-ALTER TABLE inventory_item ALTER updated_at TIMESTAMP;
+ALTER TABLE inventory_list ALTER created_at TYPE TIMESTAMP;
+ALTER TABLE inventory_list ALTER updated_at TYPE TIMESTAMP;
+ALTER TABLE inventory_item ALTER created_at TYPE TIMESTAMP;
+ALTER TABLE inventory_item ALTER updated_at TYPE TIMESTAMP;
--- goinventory-api/migrations/2_update_inventory_list_table_dates_to_bigints_not_null_all_fields.up.sql
+++ goinventory-api/migrations/2_update_inventory_list_table_dates_to_bigints_not_null_all_fields.up.sql
@@ -1,4 +1,4 @@
-ALTER TABLE inventory_list ALTER created_at BIGINT NOT NULL;
-ALTER TABLE inventory_list ALTER updated_at BIGINT NOT NULL;
-ALTER TABLE inventory_item ALTER created_at BIGINT NOT NULL;
-ALTER TABLE inventory_item ALTER updated_at BIGINT NOT NULL;
+ALTER TABLE inventory_list ALTER created_at TYPE BIGINT SET NOT NULL;
+ALTER TABLE inventory_list ALTER updated_at TYPE BIGINT SET NOT NULL;
+ALTER TABLE inventory_item ALTER created_at TYPE BIGINT SET NOT NULL;
+ALTER TABLE inventory_item ALTER updated_at TYPE BIGINT SET NOT NULL;
