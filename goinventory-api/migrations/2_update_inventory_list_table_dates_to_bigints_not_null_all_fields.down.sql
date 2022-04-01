da89168f6aef2937a75d44d84422292ce995efe1
Yongle Xu <yonglexu08@gmail.com>
2022-04-01 22:45:57 +0100

    FIX: Migration file updated to drop appropriate columns
    The time stamp columns for inventory_list and inventory_items are now dropped and recreated. I had issues with trying to directly change the data type.

--- goinventory-api/migrations/2_update_inventory_list_table_dates_to_bigints_not_null_all_fields.down.sql
+++ goinventory-api/migrations/2_update_inventory_list_table_dates_to_bigints_not_null_all_fields.down.sql
@@ -1,4 +1,15 @@
-ALTER TABLE inventory_list ALTER created_at TYPE TIMESTAMP;
-ALTER TABLE inventory_list ALTER updated_at TYPE TIMESTAMP;
-ALTER TABLE inventory_item ALTER created_at TYPE TIMESTAMP;
-ALTER TABLE inventory_item ALTER updated_at TYPE TIMESTAMP;
+ALTER TABLE inventory_list 
+DROP COLUMN IF EXISTS created_at,
+DROP COLUMN IF EXISTS updated_at;
+
+ALTER TABLE inventory_list 
+ADD COLUMN created_at BIGINT NOT NULL,
+ADD COLUMN updated_at BIGINT NOT NULL;
+
+ALTER TABLE inventory_item 
+DROP COLUMN IF EXISTS created_at,
+DROP COLUMN IF EXISTS updated_at;
+
+ALTER TABLE inventory_item
+ADD COLUMN created_at BIGINT NOT NULL,
+ADD COLUMN updated_at BIGINT NOT NULL;
