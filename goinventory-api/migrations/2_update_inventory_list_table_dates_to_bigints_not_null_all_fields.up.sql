da80eb4b2662f5488a9c1b96d73127dd4c35274f
Yongle Xu <yonglexu08@gmail.com>
2022-04-01 15:15:15 +0100

    Created at already has not null so is failing
    Removed the not null constraint set for created at for both tables.

--- goinventory-api/migrations/2_update_inventory_list_table_dates_to_bigints_not_null_all_fields.up.sql
+++ goinventory-api/migrations/2_update_inventory_list_table_dates_to_bigints_not_null_all_fields.up.sql
@@ -1,9 +1,7 @@
 ALTER TABLE inventory_list ALTER created_at TYPE BIGINT;
 ALTER TABLE inventory_list ALTER updated_at TYPE BIGINT;
-ALTER TABLE inventory_list ALTER created_at SET NOT NULL;
 ALTER TABLE inventory_list ALTER updated_at SET NOT NULL;
 
 ALTER TABLE inventory_item ALTER created_at TYPE BIGINT;
 ALTER TABLE inventory_item ALTER updated_at TYPE BIGINT;
-ALTER TABLE inventory_item ALTER created_at SET NOT NULL;
 ALTER TABLE inventory_item ALTER updated_at SET NOT NULL;
